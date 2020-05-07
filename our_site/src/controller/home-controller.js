b2531326f7bf97046c3fdc6d3972cf25e632f069
nicolekwli <nicolekwli19@gmail.com>
2020-05-07 14:05:18 +0100

    did a little organising stuff and stuff

---
+++ our_site/src/controller/home-controller.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+
+// function getBanners
+    // parses params if theres any
+    // send to service 
+        // service writes query
---
+++ our_site/src/routes/home.js
@@ -0,0 +1,9 @@
+const express = require('express');
+//import { home } from '../controllers/home';
+
+
+const homeRouter = express.Router();
+
+//homeRouter.get('/getBanners', getHomeBanners);
+
+module.exports = homeRouter;

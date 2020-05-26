f56831afdc21ebc210222db7b49b48c8a023423e
Max Whale <maxwhale99@gmail.com>
2020-05-27 00:23:20 +0100

    Shows do the thing

--- our_site/src/controllers/shows-controller.js
+++ our_site/src/controllers/shows-controller.js
@@ -3,10 +3,11 @@ const showsService = require("../service/shows-service");
 exports.getProd = async function(req, res) {
   try {
     const showData = await showsService.getProduction();
+    const shows = combineShows(showData);
         
     res.status(200).json({
       valid: true,
-      data: showData
+      data: shows
     });
 
     console.log("details received by SHOWS controller");
@@ -15,4 +16,30 @@ exports.getProd = async function(req, res) {
     res.status(400).json({errMessage: "Unable to get show data."});
   }
 };
+
+// Combine the separate entries from the inner join to one entry
+function combineShows(shows) {
+  let showMap = new Map();
+  for (const i in shows) {
+    if (!showMap.has(shows[i].production_id)) {
+      showMap.set(shows[i].production_id, i);
+      shows[i].date = [shows[i].date];
+      shows[i].doors_open = [shows[i].doors_open];
+      shows[i].total_seats = [shows[i].total_seats];
+      shows[i].sold = [shows[i].sold];
+    }
+    else {
+      const index = showMap.get(shows[i].production_id);
+      shows[index].date.push(shows[i].date);
+      shows[index].doors_open.push(shows[i].doors_open);
+      shows[index].total_seats.push(shows[i].total_seats);
+      shows[index].sold.push(shows[i].sold);
+    }
+  }
+  let result = [];
+  for (const i of showMap.values()) {
+    result.push(shows[i]);
+  }
+  return result;
+}
     
--- our_site/src/service/shows-service.js
+++ our_site/src/service/shows-service.js
@@ -1,7 +1,7 @@
 const db = require("../../database/database.js");
 
 exports.getProduction = async function(req, res) {
-  let sql = "SELECT * FROM production";
+  let sql = "SELECT * FROM production INNER JOIN show ON production.id = show.production_id";
   try {
     const rows = await db.all(sql);
     console.log(rows);

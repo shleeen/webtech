82f31a4ee57e87f8de0c3f48f5a6ab7e3859ee7a
nicolekwli <nicolekwli19@gmail.com>
2020-05-30 01:02:43 +0100

    shows navigating to individual show and back works, but nav to individual show the second time is a rip

--- our_site/public/js/shows.js
+++ our_site/public/js/shows.js
@@ -27,17 +27,19 @@ function getProductionDetails() {
         console.log("one prod done");
       }
 
-      // var prods = document.getElementsByClassName("prod-container");
-      // for (var i = 0; i < prods.length; i++) {
-      //   prods[i].addEventListener("click", displayShow);
-      // }
-       
+      var prods = document.getElementsByClassName("prod-container");
+      for (var i = 0; i < prods.length; i++) {
+        prods[i].addEventListener("click", displayShow);
+      }
+      
+      // TODO: add listeners to all the things we want to be clickable or keep it the whole thing
       // sorry i decicded to try on jsut the poster but that one line should help
       var posters = document.getElementsByClassName("prod-poster");
       for (var i = 0; i < posters.length; i++) {
         posters[i].addEventListener("click", function(){
 
           // THIS IS THE LINE <3
+          // need to add in history state
           window.parent.history.pushState("", "", "/shows/production");
  
         });
@@ -57,14 +59,22 @@ function displayShow() {
   xhr.onreadystatechange = function() {
     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) { 
       var res = xhr.response;
-      document.getElementById("shows-main").classList.add("non-active");
+
+      // TODO: display back button
+      document.getElementById("shows-return").classList.remove("non-active");
+      document.getElementById("shows-return").classList.add("active");
+
+      // slightly confused as to why removing shows-main would hide it but active and non-active doesnt
       document.getElementById("shows-main").classList.remove("shows-main");
+      document.getElementById("shows-main").classList.add("non-active");
+      
+      // this currently breaks when clicked on the second time
       document.getElementById("show-details").innerHTML = template.render("show-template", res);
 
       // still need to actually route this properly and update URL and AAAAAAAAAAAAAAAAAAAAAH
       // nicole help
       // lol i see this now, use window.parent.history.pushState()
-      window.history.pushState({}, "",  "/" + "showname");
+      window.parent.history.pushState("", "", "/shows/production");
     }
   };
 
@@ -83,5 +93,23 @@ function addShowsListeners() {
   //   //go to a different page
   //   console.log("clickk!!");
   // };
+
+
+  // TODO: Add back button listener
+    // onclick: hide back button, display list of productions
+  document.getElementById("shows-return").addEventListener("click", function () {
+    document.getElementById("shows-return").classList.remove("active");
+    document.getElementById("shows-return").classList.add("non-active");
+
+    document.getElementById("show-details").classList.add("non-active");
+
+    document.getElementById("shows-main").classList.remove("non-active");
+    document.getElementById("shows-main").classList.add("active");
+    document.getElementById("shows-main").classList.add("shows-main");
+    
+
+    // change path back to shows
+    window.parent.history.pushState("", "", "/shows");
+  })
 
 }

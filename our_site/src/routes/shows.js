ebdb5fa7b475a9771b5545161b84c77a6e24cb51
nicolekwli <nicolekwli19@gmail.com>
2020-06-02 23:08:09 +0100

    shows container more responsice

--- our_site/src/routes/shows.js
+++ our_site/src/routes/shows.js
@@ -6,6 +6,9 @@ showsRouter.get("/getProductionDetails", showsController.getProd);
 showsRouter.get("/getProductionDetails/:prodId", showsController.getProd);
 
 showsRouter.get("/getProductionSeatStatus/:prodId", showsController.getSeats)
+
+// just a guess on what we need
+showsRouter.post("/buyTickets/:showId/:seatNum/:ticketType", showsController.getSeats)
 module.exports = showsRouter;
 
 

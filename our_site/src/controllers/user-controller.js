27376a0c0c2d013967b08b1be7d45d2b88b7682e
Max Whale <maxwhale99@gmail.com>
2020-06-08 18:40:54 +0100

    Get rid of unused packages, also some formatting

--- our_site/src/controllers/user-controller.js
+++ our_site/src/controllers/user-controller.js
@@ -29,7 +29,7 @@ exports.getUserTransactions = async function(req, res) {
     const userTransactions = await userService.userTransactions(req.session.user_id);
 
     // Convert 1s and 0s to words
-    for (const u in userTransactions){
+    for (const u in userTransactions) {
       if (userTransactions[u].paid === 1)
         userTransactions[u].paid = "Paid";
       else
@@ -46,34 +46,33 @@ exports.getUserTransactions = async function(req, res) {
 
 
     // Add in ticket count 
-    for (const t in transactions){
+    for (const t in transactions) {
       transactions[t].prices = [];
       transactions[t].ticket_no = [];
 
       var current = null;
       var count = 0;
       // count the number of tickets for each price
-      for (var p in transactions[t].price){
-        if (current != transactions[t].price[p] ){
-          if (count > 0 ){
-            transactions[t].prices.push(current)
-            transactions[t].ticket_no.push(count)
+      for (var p in transactions[t].price) {
+        if (current !== transactions[t].price[p]) {
+          if (count > 0) {
+            transactions[t].prices.push(current);
+            transactions[t].ticket_no.push(count);
           }
           count = 1;
           current = transactions[t].price[p];
           
         } else count += 1;
       } 
-      if (current != null){
-        transactions[t].prices.push(current)
-        transactions[t].ticket_no.push(count)
+      if (current !== null) {
+        transactions[t].prices.push(current);
+        transactions[t].ticket_no.push(count);
 
       }
       delete transactions[t].price;
       //transactions[t].ticket_no = transactions[t].seat_number.length;
     }
     
-    console.log(transactions)
     res.status(200).json(transactions);
   } catch (err) {
     res.status(400).json({errMessage: "Unable to get user transactions."});
@@ -81,14 +80,13 @@ exports.getUserTransactions = async function(req, res) {
 };
 
 // Combine separate rows from DB to get an array of seat numbers
-function combineTransactions(trans){
+function combineTransactions(trans) {
   let transMap = new Map();
   for (const i in trans) {
-    console.log(transMap)
-    if (!transMap.has(trans[i].id)){
+    if (!transMap.has(trans[i].id)) {
       transMap.set(trans[i].id, i);
       trans[i].seat_number = [trans[i].seat_number];
-      // conbine ticket type 
+      // combine ticket type 
       trans[i].category = [trans[i].category];
       // combine ticket price
       trans[i].price = [trans[i].price];
@@ -98,14 +96,11 @@ function combineTransactions(trans){
       trans[index].seat_number.push(trans[i].seat_number);
       // check if exist
 
-      // if (!(trans[index].price.includes(trans[i].price))) {
-         trans[index].price.push(trans[i].price);
-      // }
+      trans[index].price.push(trans[i].price);
       
-       trans[index].category.push(trans[i].category);
+      trans[index].category.push(trans[i].category);
     }
   }
-
 
   let result = {};
   for (const i of transMap.values()) {
